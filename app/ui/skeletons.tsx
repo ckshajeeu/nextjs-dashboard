75a068f020fa95de4cae4012a54f76612bdd5adc
Shajeeu Abdul Razak <ckshajeeu@gmail.com>
2025-11-14 06:53:54 +0530

    add invoices page

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -165,7 +165,7 @@ export function InvoicesMobileSkeleton() {
 
 export function InvoicesTableSkeleton() {
   return (
-    <div className="mt-6 flow-root">
+    <div className="mt-6 flow-root w-full">
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
